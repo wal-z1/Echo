1f5647c41d99d31af29dd229317e500ef3c188c2
Walid <ow_bouhenika@esi.dz>
2025-10-19 22:19:04 +0100

    comments

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from pathlib import Path
 # important imports will be useful later
 app = FastAPI()
 
+#an object to store currently connected users (since no db)
+
+connected_users = {}
+
 @app.get("/")
-def home():
-    return {"message": "Hello World"}
+async def main():
+    html = Path("chat.html").read_text() # open file in that path  , and read the text
+    #return to the user the page text as an html response
+    return HTMLResponse(html)
+
+@app.websocket("/ws/{user}")
+async def websocket_endpoint(websocket: WebSocket, username: str): 
+      #import the websocket created and the user
+      await websocket.accept() # wait until the browser accepts the websocket req
