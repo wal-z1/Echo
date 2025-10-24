c2289e53e134074847c0b6bf24e416670977d893
Walid <ow_bouhenika@esi.dz>
2025-10-24 12:06:50 +0100

    making the chat room comp

--- front/echo-front/src/App.jsx
+++ front/echo-front/src/App.jsx
@@ -1,9 +1,13 @@
-
+import Chatroom from "./Chatroom";
 function App() {
-
 	return (
 		<>
-			<h1 class="text-3xl font-bold underline font-mozilla">Hello world!</h1>
+			<div className=" grid grid-rows-2 gap-3 ">
+				<h1 className="text-3xl font-bold underline font-mozilla mx-auto text-[#F5F5F5]">
+					Simple FrontEnd For ECHO chat room
+				</h1>
+				<Chatroom />
+			</div>
 		</>
 	);
 }
---
+++ front/echo-front/src/Chatroom.jsx
@@ -0,0 +1,5 @@
+import { useState, useEffect } from "react";
+
+export default function Chatroom() {
+	return <div className=" mx-auto text-2xl text-[#F5F5F5]">Chatroom</div>;
+}
